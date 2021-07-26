dc98950b70ba528e9f11f456fb341d87fa21035c
Freddy Wordingham <f.wordingham@exeter.ac.uk>
2021-07-26 16:26:10 +0100

    Added unit testing framework.

--- src/data/average.rs
+++ src/data/average.rs
@@ -5,10 +5,10 @@ use std::ops::AddAssign;
 /// Rolling average value recording.
 #[derive(Clone, Default)]
 pub struct Average {
-    /// Current average value.
-    total: f64,
     /// Total individual contributions so far.
     counts: i32,
+    /// Current average value.
+    total: f64,
 }
 
 impl Average {
@@ -17,8 +17,8 @@ impl Average {
     #[must_use]
     pub const fn new() -> Self {
         Self {
-            total: 0.0,
             counts: 0,
+            total: 0.0,
         }
     }
 
@@ -55,5 +55,32 @@ impl AddAssign<f64> for Average {
     fn add_assign(&mut self, rhs: f64) {
         self.total += rhs;
         self.counts += 1;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use assert_approx_eq::assert_approx_eq;
+
+    #[test]
+    fn test_init() {
+        let a = Average::new();
+
+        assert_eq!(a.counts, 0);
+        assert_approx_eq!(a.total, 0.0);
+    }
+
+    #[test]
+    fn test_sum() {
+        let mut a = Average::new();
+
+        for n in 0..100 {
+            a += f64::from(n);
+        }
+
+        assert_eq!(a.counts, 100);
+        assert_approx_eq!(a.total, 4950.0);
+        assert_approx_eq!(a.ave(), 49.5);
     }
 }
