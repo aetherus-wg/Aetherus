8882a1cfa03fae992c21a48994182ccbd2775a57
Freddy Wordingham <f.wordingham@exeter.ac.uk>
2021-08-02 16:10:49 +0100

    Added meta module.

--- src/lib.rs
+++ src/lib.rs
@@ -38,10 +38,5 @@
 #[cfg(feature = "data")]
 pub mod data;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+#[cfg(feature = "meta")]
+pub mod meta;
