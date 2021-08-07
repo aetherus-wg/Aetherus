4aa92474a47236fc049cd0360d49603cb21c1c80
Freddy Wordingham <f.wordingham@exeter.ac.uk>
2021-08-07 23:35:53 +0100

    Added linalg struct interfaces.

--- src/math/linalg/vec/vec3.rs
+++ src/math/linalg/vec/vec3.rs
@@ -2,8 +2,43 @@
 
 use nalgebra::Vector3;
 
-/// Three-dimensional real-number vector alias.
-pub type Vec3 = Vector3<f64>;
+/// Three-dimensional real-number vector.
+pub struct Vec3 {
+    /// Internal data.
+    data: Vector3<f64>,
+}
+
+impl Vec3 {
+    /// Construct a new instance.
+    #[inline]
+    #[must_use]
+    pub fn new(x: f64, y: f64, z: f64) -> Self {
+        Self {
+            data: Vector3::new(x, y, z),
+        }
+    }
+
+    /// Access the first component.
+    #[inline]
+    #[must_use]
+    pub fn x(&self) -> f64 {
+        return self.data.x;
+    }
+
+    /// Access the second component.
+    #[inline]
+    #[must_use]
+    pub fn y(&self) -> f64 {
+        return self.data.y;
+    }
+
+    /// Access the third component.
+    #[inline]
+    #[must_use]
+    pub fn z(&self) -> f64 {
+        return self.data.z;
+    }
+}
 
 #[cfg(test)]
 mod tests {
--- src/math/linalg/vec/vec4.rs
+++ src/math/linalg/vec/vec4.rs
@@ -2,8 +2,51 @@
 
 use nalgebra::Vector4;
 
-/// Four-dimensional real-number vector alias.
-pub type Vec4 = Vector4<f64>;
+
+/// Four-dimensional real-number vector.
+pub struct Vec4 {
+    /// Internal data.
+    data: Vector4<f64>,
+}
+
+impl Vec4 {
+    /// Construct a new instance.
+    #[inline]
+    #[must_use]
+    pub fn new(x: f64, y: f64, z: f64, w: f64) -> Self {
+        Self {
+            data: Vector4::new(x, y, z, w),
+        }
+    }
+
+    /// Access the first component.
+    #[inline]
+    #[must_use]
+    pub fn x(&self) -> f64 {
+        return self.data.x;
+    }
+
+    /// Access the second component.
+    #[inline]
+    #[must_use]
+    pub fn y(&self) -> f64 {
+        return self.data.y;
+    }
+
+    /// Access the third component.
+    #[inline]
+    #[must_use]
+    pub fn z(&self) -> f64 {
+        return self.data.z;
+    }
+
+    /// Access the fourth component.
+    #[inline]
+    #[must_use]
+    pub fn w(&self) -> f64 {
+        return self.data.w;
+    }
+}
 
 #[cfg(test)]
 mod tests {
