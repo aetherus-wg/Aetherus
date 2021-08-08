17124bc11e392252757558965ed8b317d669b8cf
Freddy Wordingham <f.wordingham@exeter.ac.uk>
2021-08-08 18:59:40 +0100

    Simplified names.

--- src/core/number/complex.rs
+++ src/core/number/complex.rs
@@ -6,14 +6,14 @@ use std::ops::{Mul};
 /// Complex number type.
 pub struct Complex {
     /// Real component.
-    real: Real,
+    re: Real,
     /// Imaginary component.
-    imag: Real,
+    im: Real,
 }
 
 impl Complex {
-    access!(real, Real);
-    access!(imag, Real);
+    access!(re, Real);
+    access!(im, Real);
 }
 
 impl Mul for Complex {
@@ -23,8 +23,8 @@ impl Mul for Complex {
     #[must_use]
     fn mul(self, rhs: Self) -> Self {
         Self {
-            real: (self.real * rhs.real) - (self.imag * rhs.imag),
-            imag: (self.real * rhs.imag) + (self.imag * rhs.real),
+            re: (self.re * rhs.re) - (self.im * rhs.im),
+            im: (self.re * rhs.im) + (self.im * rhs.re),
         }
     }
 }
