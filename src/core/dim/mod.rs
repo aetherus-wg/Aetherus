38269828b9da2c01dcb8b7e607063ba902f5734f
Freddy Wordingham <f.wordingham@exeter.ac.uk>
2021-08-12 19:58:35 +0100

    Added prefix constants.

--- src/core/dim/mod.rs
+++ src/core/dim/mod.rs
@@ -2,6 +2,8 @@
 
 pub mod physical;
 pub mod si;
+pub mod prefix;
 
 pub use self::physical::*;
+pub use self::prefix::*;
 pub use self::si::*;
--- src/core/dim/physical.rs
+++ src/core/dim/physical.rs
@@ -15,45 +15,14 @@ macro_rules! dimension {
 }
 
 // Base units
-/// Length is a measure of distance.
-/// This is a fundamental dimension.
-/// Units are the same as SI, meters.
 dimension!(Real, Length, Meter);
-
-/// Length is a measure of duration.
-/// This is a fundamental dimension.
-/// Units are the same as SI, seconds.
 dimension!(Real, Time, Second);
-
-/// Mass is a measure of resistance to acceleration within the Higgs field.
-/// This is a fundamental dimension.
-/// Units are the different to SI, as UCUM base units do not have prefixes.
-/// Units are grams.
 dimension!(Real, Mass, Gram);
-
-/// Charge is a measure of resistance to acceleration within a electromagnetic field.
-/// Units are the different to SI, as charge is considered more fundamental than current (movement of charge).
-/// This is a fundamental dimension.
-/// Units are coulombs.
 dimension!(Real, Charge, Coulomb);
-
-/// Temperature is a measure of the average kinetic energy of an ensemble of particles.
-/// This is a convenience dimension.
-/// Units are the same as SI, Kelvin.
-dimension!(Real, Temperature, Kelvin);
-
-/// Luminosity is a measure of the human eye's perceived response to light intensity.
-/// This is a convenience dimension.
-/// Units are the same as SI, candela.
-dimension!(Real, Luminosity, Candela);
-
-/// Angle is a measure of rotation required to bring one line into coincidence with another.
-/// This is a fundamental theoretical dimension.
-/// Units are radians.
+dimension!(Real, Temperature, Kelvin);dimension!(Real, Luminosity, Candela);
 dimension!(Real, Angle, Radian);
 
 // Power
 dimension!(Real, Area, Meter2);
 dimension!(Real, Volume, Meter3);
-
 dimension!(Real, Steradian, Steradian);
