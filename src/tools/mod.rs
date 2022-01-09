dd45ced4195b89e05c844115b2ff5a13d2eedb1c
Sam Morrell <samuelmorrell@gmail.com>
2022-01-09 15:53:55 +0000

    Migrated `tools` module from refactor branch.
    I have made one small change in that I have converted the `Binner` object to internally use `usize` instead of a `u64`. If this is a problem later on down the line we can always change back.

--- src/tools/mod.rs
+++ src/tools/mod.rs
@@ -1,3 +1,8 @@
+pub mod valid;
+pub mod index;
 pub mod range;
+pub mod binner;
+pub mod progress_bar;
+pub mod silent_progress_bar;
 
-pub use range::*;
+pub use {valid::*, range::*, binner::*, index::*, progress_bar::*, silent_progress_bar::*};
