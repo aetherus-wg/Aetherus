7d65745ec2ad879a57a778cf052f6917328bb627
Sam Morrell <samuelmorrell@gmail.com>
2022-01-09 16:05:51 +0000

    Added migrated files for `data` module.
    Modified `u64` in histogram to remain consistent with revised `crate::tools::Binner` implementation. Refer to issues #16 and #17 for documentation and testing tasks and progress.

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -1,5 +1,7 @@
 //! Data organisation and reduction structures.
 
 pub mod average;
+pub mod histogram;
+pub mod table;
 
-pub use self::average::*;
+pub use self::{average::*, histogram::*, table::*};
