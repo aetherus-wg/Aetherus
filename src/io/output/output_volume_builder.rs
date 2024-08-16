9e41a242d423c2590889b460ff823b28735c9224
Sam Morrell <samuelmorrell@gmail.com>
2024-08-16 16:11:46 +0100

    Improvements / bug fixes in display code.
    - Added output of LID loading to show path.
    - Fixed incorrect output of `OutputVolumBuilder` boundary.
    - Fixed warning in report macro with extraneous semicolon.

--- src/io/output/output_volume_builder.rs
+++ src/io/output/output_volume_builder.rs
@@ -30,8 +30,8 @@ impl Display for OutputVolumeBuilder {
     fn fmt(&self, fmt: &mut Formatter) -> Result<(), std::fmt::Error> {
         writeln!(fmt, "...")?;
         fmt_report!(fmt, "...", "boundary");
-        fmt_report!(fmt, format!("[{}, {}", self.boundary.0.x(), self.boundary.0.y()), "mins");
-        fmt_report!(fmt, format!("[{}, {}", self.boundary.0.x(), self.boundary.0.y()), "maxs");
+        fmt_report!(fmt, format!("[{}, {}, {}]", self.boundary.0.x(), self.boundary.0.y(), self.boundary.0.z()), "mins");
+        fmt_report!(fmt, format!("[{}, {}, {}]", self.boundary.1.x(), self.boundary.1.y(), self.boundary.1.z()), "maxs");
         fmt_report!(
             fmt,
             &format!("[{} x {} x {}]", self.res[X], self.res[Y], self.res[Z]),
