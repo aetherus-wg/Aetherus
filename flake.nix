ee9327bdb3fc6778bb5aada4d2892647e22bbcff
Cristi Bourceanu <bourceanu.cristi@gmail.com>
2025-12-17 14:59:03 +0000

    Bump rust-analyzer to be used from within the toolchain, such that it matches dev env

--- flake.nix
+++ flake.nix
@@ -1,41 +1,44 @@
 {
-  description = "sphirs - SPH in Rust";
+  description = "Aetherus Rust Development Environment";
 
   inputs = {
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    flake-utils.url  = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay }:
-    let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlaysRustToolchain = final: prev: {
+          rustToolchain =
+            let
+              rust = prev.rust-bin;
+            in
+            if builtins.pathExists ./rust-toolchain.toml then
+              rust.fromRustupToolchainFile ./rust-toolchain.toml
+            else if builtins.pathExists ./rust-toolchain then
+              rust.fromRustupToolchainFile ./rust-toolchain
+            else
+              rust.stable.latest.default.override {
+                extensions = [ "rust-src" "rustfmt" "rust-analyzer" ];
+              };
+        };
+
+        overlays = [
+          rust-overlay.overlays.default
+          overlaysRustToolchain
+        ];
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ rust-overlay.overlays.default self.overlays.default ];
+          inherit overlays;
         };
-      });
     in
     {
-      overlays.default = final: prev: {
-        rustToolchain =
-          let
-            rust = prev.rust-bin;
-          in
-          if builtins.pathExists ./rust-toolchain.toml then
-            rust.fromRustupToolchainFile ./rust-toolchain.toml
-          else if builtins.pathExists ./rust-toolchain then
-            rust.fromRustupToolchainFile ./rust-toolchain
-          else
-            rust.stable.latest.default.override {
-              extensions = [ "rust-src" "rustfmt" ];
-            };
-      };
-
-      devShells = forEachSupportedSystem ({ pkgs }: {
+      devShells = {
         default = pkgs.mkShell {
           packages = with pkgs; [
             rustToolchain
@@ -44,7 +47,6 @@
             cargo-deny
             cargo-edit
             cargo-watch
-            rust-analyzer
             netcdf
             hdf5
             mpich
@@ -61,6 +63,6 @@
             #LD_LIBRARY_PATH = "${pkgs.hdf5_1_10}/lib:${pkgs.hdf5_1_10.dev}/lib";
           };
         };
-      });
-    };
+      };
+    });
 }
