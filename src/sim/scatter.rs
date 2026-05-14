495481ae70dafe620ca8a99f205db2c7bb829c84
Cristian Bourceanu <bourceanu.cristi@gmail.com>
2026-05-14 23:50:11 +0100

    feat: absorption is now included in photon travelling

--- src/sim/scatter.rs
+++ src/sim/scatter.rs
@@ -9,10 +9,6 @@ use std::f64::consts::PI;
 
 /// Perform a photon scattering event.
 pub fn scatter<R: Rng>(rng: &mut R, phot: &mut Photon, env: &Local) {
-    // Part of the weight is absorbed.
-    *phot.weight_mut() *= env.albedo();
-
-    // The remaining weight is scattered.
     let phi = sample_henyey_greenstein(rng, env.asym());
     let theta = rng.random_range(0.0..(PI * 2.0));
     phot.ray_mut().rotate(phi, theta);
@@ -20,9 +16,6 @@ pub fn scatter<R: Rng>(rng: &mut R, phot: &mut Photon, env: &Local) {
 
 /// Perform a photon scattering event with a probability of shifting wavelength.
 pub fn shift_scatter<R: Rng>(rng: &mut R, phot: &mut Photon, env: &Local) {
-    // Part of the weight is absorbed.
-    *phot.weight_mut() *= env.albedo();
-
     // The remaining weight may be shifted in a Raman/fluorescence event.
     let r = rng.random::<f64>();
     if r <= env.shift_prob() {
